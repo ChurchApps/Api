54381bd4403eb3332bcd784ee52739751070aa19
AjayGol <ajaygoldev@gmail.com>
2026-04-08 17:30:19 +0530

    Fix Create Error Toast

--- src/modules/doing/repositories/PlanTypeRepo.ts
+++ src/modules/doing/repositories/PlanTypeRepo.ts
@@ -48,4 +48,14 @@ export class PlanTypeRepo {
       name: row.name
     };
   }
+
+  public convertToModel(_churchId: string, data: any) {
+    if (!data) return null;
+    return this.rowToModel(data);
+  }
+
+  public convertAllToModel(_churchId: string, data: any[]) {
+    if (!Array.isArray(data)) return [];
+    return data.map((d) => this.rowToModel(d));
+  }
 }
