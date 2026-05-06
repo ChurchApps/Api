ea25d87a4f0ab28e36de09823dd728c96b43cf85
Jeremy Zongker <jeremy@zongker.net>
2026-05-06 15:13:51 -0500

    Give full permission JWT

--- src/modules/membership/auth/AuthenticatedUser.ts
+++ src/modules/membership/auth/AuthenticatedUser.ts
@@ -59,6 +59,15 @@ export class AuthenticatedUser extends BaseAuthenticatedUser {
   }
 
   public static getChurchJwt(user: User, userChurch: LoginUserChurch) {
+    const permList: string[] = [];
+    userChurch.apis?.forEach((api) => {
+      api.permissions?.forEach((p) => {
+        let permString = p.contentType + "_" + String(p.contentId).replace("null", "") + "_" + p.action;
+        if (p.apiName) permString = p.apiName + "_" + p.contentType + "_" + String(p.contentId).replace("null", "") + "_" + p.action;
+        permList.push(permString);
+      });
+    });
+
     const groupIds: string[] = [];
     userChurch.groups?.forEach((g) => groupIds.push(g.id));
     const leaderGroupIds: string[] = [];
@@ -72,6 +81,7 @@ export class AuthenticatedUser extends BaseAuthenticatedUser {
         lastName: user.lastName,
         churchId: userChurch.church.id,
         personId: userChurch.person.id,
+        permissions: permList,
         groupIds,
         leaderGroupIds,
         membershipStatus: userChurch.person?.membershipStatus
